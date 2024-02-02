b34ece0bcc574fb14608735f142c6e94d8ba9063
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-02-02 11:09:29 -0800

    Created Character class

---
+++ Character.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW211_MurderMystery
+{
+    internal class Character
+    {
+        private int _characterId;
+
+        public Character(string fullName, string gender, string occupation, string script, string description, string assignedPlayer)
+        {
+            FullName = fullName;
+            Gender = gender;
+            Occupation = occupation;
+            CharScript = script;
+            Desc = description;
+            Player = assignedPlayer;
+        }
+
+        /// <summary>
+        /// Characters Id number
+        /// </summary>
+        public int CharacterId
+        {
+            get { return _characterId; }
+            set { _characterId = value; }
+        }
+
+        /// <summary>
+        /// Characters first and last name
+        /// </summary>
+        public string FullName { get; set; }
+        
+        /// <summary>
+        /// Characters gender
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Characters job
+        /// </summary>
+        public string Occupation { get; set; }
+
+        /// <summary>
+        /// Character's quotes
+        /// </summary>
+        public string CharScript { get; set; }
+
+        /// <summary>
+        /// Description of the character and their personality
+        /// </summary>
+        public string Desc { get; set; }
+
+        /// <summary>
+        /// ?
+        /// </summary>
+        public string Player { get; set; }
+
+    }
+}
