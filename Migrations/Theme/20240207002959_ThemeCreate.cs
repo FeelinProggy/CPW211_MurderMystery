b39fc4c2f127df51c1f5e28cfb8b082cc54d46e2
Jennifer Peterson <jennpeterson986@gmail.com>
2024-02-06 16:34:31 -0800

    Added Migration for Theme

---
+++ Migrations/Theme/20240207002959_ThemeCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CPW211_MurderMystery.Migrations.Theme
+{
+    /// <inheritdoc />
+    public partial class ThemeCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Themes",
+                columns: table => new
+                {
+                    ThemeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Themes", x => x.ThemeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Themes");
+        }
+    }
+}
