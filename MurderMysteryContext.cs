aa77cd70c6b285362e54c3526be3ee80c31d88b3
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-02-13 11:58:42 -0800

    Added Themes and Characters to DbContext

--- MurderMysteryContext.cs
+++ MurderMysteryContext.cs
@@ -18,5 +18,7 @@ namespace CPW211_MurderMystery
         }
 
         public DbSet<Player> Players { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Theme> Themes { get; set; }
     }
 }
