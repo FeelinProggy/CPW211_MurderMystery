a779964f3705b634361101195ca816c9d982215d
DZ <zimmerli1987@students.cptc.edu>
2024-02-13 13:48:09 -0800

    Added code to populate the List Box in the PartyCreatorForm

--- PartyCreatorForm.cs
+++ PartyCreatorForm.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace CPW211_MurderMystery
 {
     public partial class PartyCreatorForm : Form
@@ -5,13 +8,37 @@ namespace CPW211_MurderMystery
         public PartyCreatorForm()
         {
             InitializeComponent();
+            PopulateListBox();
         }
 
+        /// <summary>
+        /// Clears "lstPlayers" then populates it with data from the Players table.
+        /// </summary>
+        public void PopulateListBox()
+        {
+            lstPlayers.Items.Clear();
 
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+            using (var context = new MurderMysteryContext())
+            {
+                var player = context.Players.Select(p => new {p.PlayerFullName, p.PlayerGender}).ToList();
+
+                foreach (var playerData in player)
+                {
+                    string listItem = $"{playerData.PlayerFullName} : {playerData.PlayerGender}";
+                    lstPlayers.Items.Add(listItem);
+                }
+            }
+        }
+
+        private void btnAddPlayers_Click(object sender, EventArgs e)
+        {
+            AddPlayerForm addPlayer = new AddPlayerForm();
+            addPlayer.ShowDialog();
+        }
+
+        private void btnRemovePlayers_Click(object sender, EventArgs e)
         {
 
         }
-
     }
 }
