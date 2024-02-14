984801f770e18212ee482f4769c48fdd58ecad06
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2024-02-14 13:41:31 -0800

    Added a Test form

---
+++ Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPW211_MurderMystery
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
