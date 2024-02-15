60a8720319aa0ee99956c92a3ce7d3354b737b22
Jennifer Peterson <jennpeterson986@gmail.com>
2024-02-15 13:43:10 -0800

    Created new Initial Migration

--- Migrations/20240215213356_Initial.cs
+++ Migrations/20240215213356_Initial.cs
@@ -2,10 +2,12 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace CPW211_MurderMystery.Migrations
 {
     /// <inheritdoc />
-    public partial class Intial : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -53,6 +55,32 @@ namespace CPW211_MurderMystery.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Themes", x => x.ThemeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "CharacterId", "CharScript", "Desc", "FullName", "Gender", "Occupation" },
+                values: new object[,]
+                {
+                    { 1, "TDB", "Father of Deimos, Phobos and Eden", "Mars Olufemi", "Male", "Cartographer" },
+                    { 2, "TDB", "Son of Mars, twin brother of Phobos, and elder brother of Eden.", "Deimos Olufemi", "Male", "Singer" },
+                    { 3, "TDB", "Son of Mars, twin brother of Deimos, and elder brother of Eden.", "Phobos Olufemi", "Male", "Locksmith" },
+                    { 4, "TDB", "Adopted daughter of Mars and younger sister of Deimos and Phobos.", "Eden Olufemi", "Female", "Musician" },
+                    { 5, "TDB", "Best friend of Phoebe.", "Tahlia Collins", "Female", "Gardener" },
+                    { 6, "TDB", "Best friend of Tahlia.", "Phoebe Mandrapilias", "Female", "Painter" },
+                    { 7, "TDB", "Wife/business partner of Walter and mother of Jamie.", "Silvia Honey", "Female", "Shop Owner" },
+                    { 8, "TDB", "Husband/business partner of Silvia and father of Jamie.", "Walter Honey", "Male", "Shop Owner" },
+                    { 9, "TDB", "Son of Walter and Silvia. Jamie was often babysat by Deimos as a child, and the two are still very close to this day. Jamie is also friends with Elliot, and often spends time in his library. Although, while the two enjoy talking, Jamie can’t say he knows much about Elliot personally.", "Jamie Honey", "Male", "Waiter" },
+                    { 10, "TDB", "Friend of Jamie, but much of his past is a mystery to Jamie.", "Elliot Perry", "Male", "Librarian" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Themes",
+                columns: new[] { "ThemeId", "Summary", "Title" },
+                values: new object[,]
+                {
+                    { 1, "The town business tycoon is hosting a mascarade at her manor to celebrate her sons taking over of the family company.", "Mansion Party" },
+                    { 2, "This train is on a two hour nonstop trip to the next city over.As this specific route goes through the countryside, phone connections are spotty at best.", "Train" }
                 });
         }
 
