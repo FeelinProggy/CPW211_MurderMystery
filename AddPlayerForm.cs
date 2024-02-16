ba5ae2973c45971a2d062812d216164c777a7d5e
DZ <zimmerli1987@students.cptc.edu>
2024-02-15 17:55:56 -0800

    Revert Indentation back to Spaces

--- AddPlayerForm.cs
+++ AddPlayerForm.cs
@@ -2,119 +2,119 @@
 
 namespace CPW211_MurderMystery
 {
-	public partial class AddPlayerForm : Form
-	{
-		public AddPlayerForm()
-		{
-			InitializeComponent();
-		}
+    public partial class AddPlayerForm : Form
+    {
+        public AddPlayerForm()
+        {
+            InitializeComponent();
+        }
 
-		/// <summary>
-		/// Check if the Player Name is valid.
-		/// </summary>
-		/// <returns>True if valid.</returns>
-		private bool ValidatePlayerName()
-		{
-			if (string.IsNullOrWhiteSpace(txtPlayerName.Text))
-			{
-				MessageBox.Show("Please enter a name.");
+        /// <summary>
+        /// Check if the Player Name is valid.
+        /// </summary>
+        /// <returns>True if valid.</returns>
+        private bool ValidatePlayerName()
+        {
+            if (string.IsNullOrWhiteSpace(txtPlayerName.Text))
+            {
+                MessageBox.Show("Please enter a name.");
 
-				return false;
-			}
+                return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		/// <summary>
-		/// Finds which radio button is Checked. If no radio button is Checked,
-		/// set to "No preference".
-		/// </summary>
-		/// <returns>Returns the selected Gender.</returns>
-		private string SelectedPlayerGender()
-		{
-			foreach (Control control in this.Controls)
-			{
-				if (control is RadioButton)
-				{
-					RadioButton radioButton = control as RadioButton;
-					if (radioButton.Checked)
-					{
-						return radioButton.Text;
-					}
-				}
-			}
+        /// <summary>
+        /// Finds which radio button is Checked. If no radio button is Checked,
+        /// set to "No preference".
+        /// </summary>
+        /// <returns>Returns the selected Gender.</returns>
+        private string SelectedPlayerGender()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radioButton = control as RadioButton;
+                    if (radioButton.Checked)
+                    {
+                        return radioButton.Text;
+                    }
+                }
+            }
 
-			return "No preference";
-		}
+            return "No preference";
+        }
 
-		/// <summary>
-		/// Adds Player to the database.
-		/// </summary>
-		private void AddPlayer()
-		{
-			Player p = new()
-			{
-				PlayerFullName = txtPlayerName.Text,
-				PlayerGender = SelectedPlayerGender()
-			};
+        /// <summary>
+        /// Adds Player to the database.
+        /// </summary>
+        private void AddPlayer()
+        {
+            Player p = new()
+            {
+                PlayerFullName = txtPlayerName.Text,
+                PlayerGender = SelectedPlayerGender()
+            };
 
-			MurderMysteryContext context = new();
-			context.Players.Add(p);
-			context.SaveChanges();
-		}
+            MurderMysteryContext context = new();
+            context.Players.Add(p);
+            context.SaveChanges();
+        }
 
-		/// <summary>
-		/// Updates "lstPlayers" in the PartyCreatorForm.
-		/// </summary>
-		private static void UpdateListBox()
-		{
-			(Application.OpenForms["PartyCreatorForm"] as PartyCreatorForm).PopulateListBox();
-		}
+        /// <summary>
+        /// Updates "lstPlayers" in the PartyCreatorForm.
+        /// </summary>
+        private static void UpdateListBox()
+        {
+            (Application.OpenForms["PartyCreatorForm"] as PartyCreatorForm).PopulateListBox();
+        }
 
-		/// <summary>
-		/// Resets the form.
-		/// </summary>
-		private void ResetForm()
-		{
-			txtPlayerName.Text = string.Empty;
-			rdoPlayerMale.Checked = false;
-			rdoPlayerFemale.Checked = false;
-			rdoPlayerNone.Checked = false;
-		}
+        /// <summary>
+        /// Resets the form.
+        /// </summary>
+        private void ResetForm()
+        {
+            txtPlayerName.Text = string.Empty;
+            rdoPlayerMale.Checked = false;
+            rdoPlayerFemale.Checked = false;
+            rdoPlayerNone.Checked = false;
+        }
 
-		/// <summary>
-		/// Adds Player to the database and resets the form.
-		/// </summary>
-		private void btnSubmitMulti_Click(object sender, EventArgs e)
-		{
-			if (ValidatePlayerName())
-			{
-				AddPlayer();
-				UpdateListBox();
-				ResetForm();
-			}
-		}
+        /// <summary>
+        /// Adds Player to the database and resets the form.
+        /// </summary>
+        private void btnSubmitMulti_Click(object sender, EventArgs e)
+        {
+            if (ValidatePlayerName())
+            {
+                AddPlayer();
+                UpdateListBox();
+                ResetForm();
+            }
+        }
 
-		/// <summary>
-		/// Adds Player to the database and closes the form.
-		/// </summary>
-		private void btnSubmitSingle_Click(object sender, EventArgs e)
-		{
-			if (ValidatePlayerName())
-			{
-				AddPlayer();
-				UpdateListBox();
-				Close();
-			}
-		}
+        /// <summary>
+        /// Adds Player to the database and closes the form.
+        /// </summary>
+        private void btnSubmitSingle_Click(object sender, EventArgs e)
+        {
+            if (ValidatePlayerName())
+            {
+                AddPlayer();
+                UpdateListBox();
+                Close();
+            }
+        }
 
-		/// <summary>
-		/// Closes the form and updates "lstPlayers" in PartyCreatorForm.
-		/// </summary>
-		private void btnClose_Click(object sender, EventArgs e)
-		{
-			UpdateListBox();
-			Close();
-		}
-	}
+        /// <summary>
+        /// Closes the form and updates "lstPlayers" in PartyCreatorForm.
+        /// </summary>
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            UpdateListBox();
+            Close();
+        }
+    }
 }
