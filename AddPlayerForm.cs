9c4254f50880836806bd70820e1b2da7c5bbaae8
DZ <zimmerli1987@students.cptc.edu>
2024-02-15 17:05:21 -0800

    Simplified code in AddPlayerForm and PartyCreatorForm

--- AddPlayerForm.cs
+++ AddPlayerForm.cs
@@ -2,118 +2,119 @@
 
 namespace CPW211_MurderMystery
 {
-    public partial class AddPlayerForm : Form
-    {
-        public AddPlayerForm()
-        {
-            InitializeComponent();
-        }
+	public partial class AddPlayerForm : Form
+	{
+		public AddPlayerForm()
+		{
+			InitializeComponent();
+		}
 
-        /// <summary>
-        /// Check if the Player Name is valid.
-        /// </summary>
-        /// <returns>True if valid.</returns>
-        private bool ValidatePlayerName()
-        {
-            if (string.IsNullOrWhiteSpace(txtPlayerName.Text))
-            {
-                MessageBox.Show("Please enter a name.");
-                return false;
-            }
-            return true;
-        }
+		/// <summary>
+		/// Check if the Player Name is valid.
+		/// </summary>
+		/// <returns>True if valid.</returns>
+		private bool ValidatePlayerName()
+		{
+			if (string.IsNullOrWhiteSpace(txtPlayerName.Text))
+			{
+				MessageBox.Show("Please enter a name.");
 
-        /// <summary>
-        /// Finds which radio button is Checked.
-        /// </summary>
-        /// <returns>Returns the selected Gender.</returns>
-        private string PlayerGenderSelected()
-        {
-            foreach (Control control in this.Controls)
-            {
-                if (control is RadioButton)
-                {
-                    RadioButton radioButton = control as RadioButton;
-                    if (radioButton.Checked)
-                    {
-                        return radioButton.Text.ToLower();
-                    }
-                }
-            }
+				return false;
+			}
 
-            return "no preference";
-        }
+			return true;
+		}
 
-        /// <summary>
-        /// Adds Player to database.
-        /// </summary>
-        private void AddPlayer()
-        {
-            Player p = new()
-            {
-                PlayerFullName = txtPlayerName.Text,
-                PlayerGender = PlayerGenderSelected()
-            };
+		/// <summary>
+		/// Finds which radio button is Checked. If no radio button is Checked,
+		/// set to "No preference".
+		/// </summary>
+		/// <returns>Returns the selected Gender.</returns>
+		private string SelectedPlayerGender()
+		{
+			foreach (Control control in this.Controls)
+			{
+				if (control is RadioButton)
+				{
+					RadioButton radioButton = control as RadioButton;
+					if (radioButton.Checked)
+					{
+						return radioButton.Text;
+					}
+				}
+			}
 
-            MurderMysteryContext context = new();
-            context.Players.Add(p);
-            context.SaveChanges();
-        }
+			return "No preference";
+		}
 
-        /// <summary>
-        /// Updates "lstPlayers" in the PartyCreatorForm.
-        /// </summary>
-        private static void UpdateListBox()
-        {
-            (Application.OpenForms["PartyCreatorForm"] as PartyCreatorForm).PopulateListBox();
-        }
+		/// <summary>
+		/// Adds Player to the database.
+		/// </summary>
+		private void AddPlayer()
+		{
+			Player p = new()
+			{
+				PlayerFullName = txtPlayerName.Text,
+				PlayerGender = SelectedPlayerGender()
+			};
 
-        /// <summary>
-        /// Resets the form.
-        /// </summary>
-        private void ResetForm()
-        {
-            txtPlayerName.Text = "";
-            rdoPlayerMale.Checked = false;
-            rdoPlayerFemale.Checked = false;
-            rdoPlayerNone.Checked = false;
-        }
+			MurderMysteryContext context = new();
+			context.Players.Add(p);
+			context.SaveChanges();
+		}
 
-        /// <summary>
-        /// Adds Player to database and resets the form.
-        /// </summary>
-        private void btnSubmitMulti_Click(object sender, EventArgs e)
-        {
-            bool isValid = ValidatePlayerName();
-            if (isValid)
-            {
-                AddPlayer();
-                UpdateListBox();
-                ResetForm();
-            }
-        }
+		/// <summary>
+		/// Updates "lstPlayers" in the PartyCreatorForm.
+		/// </summary>
+		private static void UpdateListBox()
+		{
+			(Application.OpenForms["PartyCreatorForm"] as PartyCreatorForm).PopulateListBox();
+		}
 
-        /// <summary>
-        /// Adds Player to database and closes the form.
-        /// </summary>
-        private void btnSubmitSingle_Click(object sender, EventArgs e)
-        {
-            bool isValid = ValidatePlayerName();
-            if (isValid)
-            {
-                AddPlayer();
-                UpdateListBox();
-                Close();
-            }
-        }
+		/// <summary>
+		/// Resets the form.
+		/// </summary>
+		private void ResetForm()
+		{
+			txtPlayerName.Text = string.Empty;
+			rdoPlayerMale.Checked = false;
+			rdoPlayerFemale.Checked = false;
+			rdoPlayerNone.Checked = false;
+		}
 
-        /// <summary>
-        /// Closes the form and updates "lstPlayers" in PartyCreatorForm.
-        /// </summary>
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            UpdateListBox();
-            Close();
-        }
-    }
+		/// <summary>
+		/// Adds Player to the database and resets the form.
+		/// </summary>
+		private void btnSubmitMulti_Click(object sender, EventArgs e)
+		{
+			if (ValidatePlayerName())
+			{
+				AddPlayer();
+				UpdateListBox();
+				ResetForm();
+			}
+		}
+
+		/// <summary>
+		/// Adds Player to the database and closes the form.
+		/// </summary>
+		private void btnSubmitSingle_Click(object sender, EventArgs e)
+		{
+			if (ValidatePlayerName())
+			{
+				AddPlayer();
+				UpdateListBox();
+				Close();
+			}
+		}
+
+		/// <summary>
+		/// Closes the form and updates "lstPlayers" in PartyCreatorForm.
+		/// </summary>
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			UpdateListBox();
+			Close();
+		}
+	}
 }
--- PartyCreatorForm.cs
+++ PartyCreatorForm.cs
@@ -1,85 +1,78 @@
-using Microsoft.Data.SqlClient;
-using System.Windows.Forms;
-
 namespace CPW211_MurderMystery
 {
-    public partial class PartyCreatorForm : Form
-    {
-        public PartyCreatorForm()
-        {
-            InitializeComponent();
-            PopulateListBox();
-        }
+	public partial class PartyCreatorForm : Form
+	{
+		public PartyCreatorForm()
+		{
+			InitializeComponent();
+			PopulateListBox();
+		}
 
-        /// <summary>
-        /// Clears "lstPlayers" then populates it with data from the Players table.
-        /// </summary>
-        public void PopulateListBox()
-        {
-            lstPlayers.Items.Clear();
+		/// <summary>
+		/// Clears "lstPlayers" then populates it with data from the Players table.
+		/// </summary>
+		public void PopulateListBox()
+		{
+			lstPlayers.Items.Clear();
 
-            using (var context = new MurderMysteryContext())
-            {
-                var player = context.Players.Select(p => new {p.PlayerFullName, p.PlayerGender}).ToList();
+			using MurderMysteryContext context = new();
+			var player = context.Players.Select(p => new { p.PlayerFullName, p.PlayerGender }).ToList();
 
-                foreach (var playerData in player)
-                {
-                    string listItem = $"{playerData.PlayerFullName} : {playerData.PlayerGender}";
-                    lstPlayers.Items.Add(listItem);
-                }
-            }
-        }
+			foreach (var playerData in player)
+			{
+				string listItem = $"{playerData.PlayerFullName} : {playerData.PlayerGender}";
+				lstPlayers.Items.Add(listItem);
+			}
+		}
 
-        /// <summary>
-        /// Removes the selected Player from the database.
-        /// </summary>
-        /// <param name="playerToRemove"></param>
-        private void RemovePlayer(string playerToRemove)
-        {
-            lstPlayers.Items.RemoveAt(lstPlayers.SelectedIndex);
+		/// <summary>
+		/// Removes the selected Player from the database.
+		/// </summary>
+		/// <param name="playerToRemove"></param>
+		private void RemovePlayer(string playerToRemove)
+		{
+			lstPlayers.Items.RemoveAt(lstPlayers.SelectedIndex);
 
-            using (var context = new MurderMysteryContext())
-            {
-                var player = context.Players.FirstOrDefault(p => p.PlayerFullName == playerToRemove);
+			using MurderMysteryContext context = new();
+			Player player = context.Players.FirstOrDefault(p => p.PlayerFullName == playerToRemove);
 
-                if (player != null)
-                {
-                    context.Players.Remove(player);
-                    context.SaveChanges();
-                }
-            }
-        }
+			if (player != null)
+			{
+				context.Players.Remove(player);
+				context.SaveChanges();
+			}
+		}
 
-        /// <summary>
-        /// Trims "lstPlayers" selected item so only the PlayerFullName is left.
-        /// </summary>
-        /// <returns>Returns a trimmed version of the string.</returns>
-        private string PlayerToRemoveTrim()
-        {
-            string playerToRemove = lstPlayers.SelectedItem.ToString();
-            string trimCharacter = " :";
-            int index = playerToRemove.IndexOf(trimCharacter);
-            if (index >= 0)
-            {
-                playerToRemove = playerToRemove.Substring(0, index);
-            }
+		/// <summary>
+		/// Trims "lstPlayers" selected item so only the Player Name is left.
+		/// </summary>
+		/// <returns>Returns a trimmed version of the string.</returns>
+		private string PlayerToRemoveTrim()
+		{
+			string playerToRemove = lstPlayers.SelectedItem.ToString();
+			string trimCharacter = " :";
+			int index = playerToRemove.IndexOf(trimCharacter);
+			if (index >= 0)
+			{
+				playerToRemove = playerToRemove.Substring(0, index);
+			}
 
-            return playerToRemove;
-        }
+			return playerToRemove;
+		}
 
-        private void btnAddPlayers_Click(object sender, EventArgs e)
-        {
-            AddPlayerForm addPlayer = new AddPlayerForm();
-            addPlayer.ShowDialog();
-        }
+		private void btnAddPlayers_Click(object sender, EventArgs e)
+		{
+			AddPlayerForm addPlayer = new AddPlayerForm();
+			addPlayer.ShowDialog();
+		}
 
-        private void btnRemovePlayers_Click(object sender, EventArgs e)
-        {
-            if (lstPlayers.SelectedIndex != -1)
-            {
-                string playerToRemove = PlayerToRemoveTrim();
-                RemovePlayer(playerToRemove);
-            }
-        }
-    }
+		private void btnRemovePlayers_Click(object sender, EventArgs e)
+		{
+			if (lstPlayers.SelectedIndex != -1)
+			{
+				string playerToRemove = PlayerToRemoveTrim();
+				RemovePlayer(playerToRemove);
+			}
+		}
+	}
 }
