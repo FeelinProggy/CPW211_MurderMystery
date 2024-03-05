0a7cd7523cc35ca4a63aebdcd78d02d7df27d720
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-04 20:13:33 -0800

    Created new class for character assiignments

---
+++ CastAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW211_MurderMystery
+{
+    public class CastAssignment
+    {
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public int CharacterId { get; set; }
+        public Character Character { get; set; }
+    }
+}
