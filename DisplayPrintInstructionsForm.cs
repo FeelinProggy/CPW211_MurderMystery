d2af828595591ea88224b1d41874e9c88b540e6b
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-05 21:58:15 -0800

    Refactored use of MurderMysteryContext in DisplayPrintInstructionsForm

--- DisplayPrintInstructionsForm.cs
+++ DisplayPrintInstructionsForm.cs
@@ -3,33 +3,31 @@
     public partial class DisplayPrintInstructionsForm : Form
     {
         private Theme _theme;
+        private MurderMysteryContext _context;
 
         public DisplayPrintInstructionsForm(Theme theme)
         {
             InitializeComponent();
             _theme = theme;
+            _context = new MurderMysteryContext();
             this.FormClosed += DisplayPrintInstructionsForm_FormClosed;
-        }
-
-        private void DisplayPrintInstructionsForm_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            using MurderMysteryContext context = new();
-            context.ResetAssignedCharacterId();
         }
 
         private void DisplayPrintInstructionsForm_Load(object sender, EventArgs e)
         {
             rtbThemeSelection.Text = $"{_theme.Title}: {_theme.Summary}";
-
-            using (var context = new MurderMysteryContext())
-            {
-                CastAssignment.AssignCharacters(context, _theme.ThemeId);
-            }
+            CastAssignment.AssignCharacters(_context, _theme.ThemeId);
         }
 
         private void btnCloseResults_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void DisplayPrintInstructionsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _context.ResetAssignedCharacterId();
+            _context.Dispose();
         }
     }
 }
