9656fa6fa0576138b42eb4c312da11f65bebd833
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-05 16:51:38 -0800

    Added class for character assignment

--- CastAssignment.cs
+++ CastAssignment.cs
@@ -8,10 +8,44 @@ namespace CPW211_MurderMystery
 {
     public class CastAssignment
     {
-        public int PlayerId { get; set; }
-        public Player Player { get; set; }
+        
+        public static void AssignCharacters(MurderMysteryContext context, int selectedThemeId)
+        {
+            var players = context.Players.ToList();
+            var characters = context.Characters
+                .Where(c => c.ThemeId == selectedThemeId)
+                .ToList();
 
-        public int CharacterId { get; set; }
-        public Character Character { get; set; }
+            foreach (var player in players)
+            {
+                var genderPreference = player.GenderPreference;
+                var availableCharacters = characters.Where(c => c.Gender == genderPreference && !context.Players.Any(p => p.AssignedCharacterId == c.CharacterId)).ToList();
+
+                Character assignedCharacter = null;
+                if (availableCharacters.Any())
+                {
+                    // Assign character based on gender preference
+                    assignedCharacter = availableCharacters[new Random().Next(availableCharacters.Count)];
+                }
+                else
+                {
+                    // If no characters of the preferred gender are available, assign any remaining character
+                    var remainingCharacters = characters.Where(c => !context.Players.Any(p => p.AssignedCharacterId == c.CharacterId)).ToList();
+                    if (remainingCharacters.Any())
+                    {
+                        assignedCharacter = remainingCharacters[new Random().Next(remainingCharacters.Count)];
+                    }
+                }
+
+                if (assignedCharacter != null)
+                {
+                    player.AssignedCharacterId = assignedCharacter.CharacterId;
+                    characters.Remove(assignedCharacter); // Remove the assigned character from the list
+                }
+            }
+
+            context.SaveChanges();
+        }
+
     }
 }
